3bfee59003fd99c9b20f4bc47841d46070e485db
Amaunat <delasalle.sio.causer.a@gmail.com>
2018-03-15 15:00:12 +0100

    Hopa

--- app/src/main/java/amaury/android/sudoku/Dessin.java
+++ app/src/main/java/amaury/android/sudoku/Dessin.java
@@ -5,41 +5,98 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 /**
  * Created by amaury on 15/03/2018.
  */
 
-class Cercle {
-    int xc, yc, rayon;
-    private Paint paint;
+class Care  {
+    Float r,l,t,b;
+    public Paint paint;
 
-    public Cercle(int x, int y, int r) {
-        xc = x;
-        yc = y;
-        rayon = r;
+
+
+    public Care(Float l, Float t, Float r, Float b) {
+        this.l = l;
+        this.r = r;
+        this.t = t;
+        this.b = b;
         paint = new Paint();
         paint.setColor(Color.rgb((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256)));
     }
     public void draw(Canvas canvas)
     {
-        canvas.drawCircle(xc, yc, rayon, paint);
+
+        canvas.drawRect(l,t,r,b,paint);
+
     }
+
+
+
+}
+class Chifrage  {
+    String chaine;
+    private Paint paint;
+
+
+
+    public Chifrage(String chaine, Paint paint) {
+        chaine= this.chaine;
+        paint = new Paint();
+        paint.setColor(Color.rgb((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256)));
+    }
+    public void draw(Canvas canvas)
+    {
+
+        canvas.drawText(l,t,r,b,paint);
+
+    }
+
+
+
 }
 
 public class Dessin extends View {
+    Paint paint2= new Paint();
     public Dessin(Context context, AttributeSet attrs) {
-        super(context);
+        super(context,attrs);
+
     }
     @Override
     public void onDraw(Canvas canvas) {
-        /*
-        paint.setColor(Color.BLUE);
-        canvas.drawCircle(100, 100, 50, paint);*/
-        (new Cercle(100,100,50)).draw(canvas    );
+
+        super.onDraw(canvas);
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+        int margeW = width / 9;
+        int margeH = width / 9;
+        float l,r,t,b;
+        l= 0;r=l+margeW;t=0;b=t+margeH;
+        for(int i = 0;i<9;i++){
+            for(int j = 0;j<9;j++) {
+                new Care(l, t, r , b).draw(canvas);
+                l+=margeW;r+=margeW;
+
+
+            }
+            l=0;r=l+margeW;
+            b+=margeH;t+=margeH;
+        }
+
+
+
     }
 
-}
+
+
+  }
 
--- app/src/main/java/amaury/android/sudoku/MainActivity.java
+++ app/src/main/java/amaury/android/sudoku/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int Grille[][]= new int[8][8];
+        /*int Grille[][]= new int[8][8];
         String chaine_sudoku = "008203500009670408346050702430010059967005001000496203280034067703500904004107020";
         int compteur=0;
         for (int i = 0;i <=8;i++){
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
                 Grille[i][j] = Integer.parseInt(chaine_sudoku.substring(compteur));
                 compteur++;
             }
-        }
+        }*/
 
 
     }
